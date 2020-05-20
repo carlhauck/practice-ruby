c6af9988bdfc2078c427e2a728b1d2cf020d9bed
carlhauck <carlhauck@gmail.com>
2020-05-20 12:35:26 -0500

    adds store item file

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+pool_noodle = {price: 12, color: "hot pink", material: "lead", quantity: 30}
+plastic_flamingo = {price: 21.99, color: "pink", material: "plastic", quantity: 42}
+live_donkey = {:price => 2000, :color => "gray", :material => "organic fur", quantity: 2}
