0939638aee65ecc001988d19e6a1a9ea496e6fc3
carlhauck <carlhauck@gmail.com>
2020-05-20 14:32:27 -0500

    changes initialize input from multiple paramaters to hash with multiple symbols

--- store_item.rb
+++ store_item.rb
@@ -4,20 +4,20 @@
 
 class Merch
 
-  def initialize(input_price, input_color, input_material, input_quantity)
-    @price = input_price
-    @color = input_color
-    @material = input_material
-    @quantity = input_quantity
+  def initialize(input_hash)
+    @price = input_hash[:price]
+    @color = input_hash[:color]
+    @material = input_hash[:material]
+    @quantity = input_hash[:quantity]
   end
 
   attr_accessor :price, :color, :material, :quantity
 
 end
 
-pool_noodle = Merch.new(12, "hot pink", "lead", 30)
-plastic_flamingo = Merch.new(21.99, "pink", "plastic", 42)
-live_donkey = Merch.new(2000, "gray", "organic fur", 2)
+pool_noodle = Merch.new({price: 12, color: "hot pink", material: "lead", quantity: 30})
+plastic_flamingo = Merch.new({price: 21.99, color: "pink", material: "plastic", quantity: 42})
+live_donkey = Merch.new(price: 2000, color: "gray", material: "organic fur", quantity: 2)
 
 puts pool_noodle.price
 puts pool_noodle.color
